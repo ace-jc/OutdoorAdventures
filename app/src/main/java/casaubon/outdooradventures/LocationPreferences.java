a7f114acac66ef5d00d4541e455081ae338c3736
jc <ace-jc@users.noreply.github.com>
2016-07-22 01:23:20 -0500

    fixed issue with misalignment between activities. Resized state and activity boxes.

--- app/src/main/java/casaubon/outdooradventures/LocationPreferences.java
+++ app/src/main/java/casaubon/outdooradventures/LocationPreferences.java
@@ -141,13 +141,4 @@ public class LocationPreferences extends ListActivity {
             return(row);
         }
     }
-
-    //TODO: Add the menu on this activity
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.result_list_menu, menu);
-//        return true;
-//    }
-
 }
