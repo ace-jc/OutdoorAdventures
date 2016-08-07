e3813d04cdad249e49cc79a0ecbf96fa5a7055cf
jc <ace-jc@users.noreply.github.com>
2016-08-07 13:58:23 -0500

    Places is now incorporated in the Park Details page

--- app/src/main/java/casaubon/outdooradventures/ParkDetail.java
+++ app/src/main/java/casaubon/outdooradventures/ParkDetail.java
@@ -16,10 +16,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.drive.query.Filter;
 import com.google.android.gms.location.places.AutocompleteFilter;
-import com.google.android.gms.location.places.AutocompletePrediction;
 import com.google.android.gms.location.places.AutocompletePredictionBuffer;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.PlaceBuffer;
@@ -29,15 +26,8 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 public class ParkDetail extends AppCompatActivity implements OnMapReadyCallback, OnConnectionFailedListener {
 
     // private variables
@@ -45,6 +35,11 @@ public class ParkDetail extends AppCompatActivity implements OnMapReadyCallback,
     private OutdoorDetails selectedPark;
     GoogleMap mMap;
     private GoogleApiClient mGoogleApiClient;
+    TextView parkName;
+    TextView parkPhone;
+    TextView parkAddress;
+    TextView amenitiesList;
+
 
 
     @Override
@@ -62,11 +57,15 @@ public class ParkDetail extends AppCompatActivity implements OnMapReadyCallback,
         selectedPark = (OutdoorDetails) getIntent().getParcelableExtra("mSewerHookup");
         selectedPark = (OutdoorDetails) getIntent().getParcelableExtra("mWaterHookup");
         selectedPark = (OutdoorDetails) getIntent().getParcelableExtra("mWaterFront");
-        TextView parkName = (TextView)findViewById(R.id.textView2);
+        // setting up the textviews
+        parkName = (TextView)findViewById(R.id.textView1);
+        parkPhone = (TextView)findViewById(R.id.textView2);
+        parkAddress = (TextView)findViewById(R.id.textView3);
+        amenitiesList = (TextView)findViewById(R.id.textView4);
+        // setting the actual text
         parkName.setText(selectedPark.getName());
-        TextView cityStateDistance = (TextView)findViewById(R.id.textView7);
-        cityStateDistance.setText("City, " + selectedPark.getState());
-        TextView amenitiesList = (TextView)findViewById(R.id.textView8);
+        parkPhone.setText("Phone number: (not available)");
+        parkAddress.setText("Address: " + selectedPark.getState());
         amenitiesList.setText("Amenities: " + selectedPark.amenitiesList());
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -85,7 +84,7 @@ public class ParkDetail extends AppCompatActivity implements OnMapReadyCallback,
 
         Log.i(TAG, "We are sending in the following park : " + selectedPark.getName());
         DisplayResults resultsOfQuery = new DisplayResults();
-        resultsOfQuery.execute(selectedPark.getName());
+        resultsOfQuery.execute(selectedPark.getName() + selectedPark.getState());
         Log.i(TAG, "AFTER : " + selectedPark.getName());
 
     }
@@ -100,15 +99,20 @@ public class ParkDetail extends AppCompatActivity implements OnMapReadyCallback,
             PendingResult<AutocompletePredictionBuffer> results = Places.GeoDataApi.getAutocompletePredictions(mGoogleApiClient, params[0], null, filter);
             AutocompletePredictionBuffer autocompletePredictions = results.await();
             Log.i(TAG, "Number in doInBackground: " + autocompletePredictions.getCount());
-            for(int i=0; i<autocompletePredictions.getCount(); ++i){
+            return autocompletePredictions.get(0).getPlaceId();
+        }
 
-                Log.i(TAG, "ENTRY NUMBER: " + i);
-                Places.GeoDataApi.getPlaceById(mGoogleApiClient, autocompletePredictions.get(i).getPlaceId()) // getPlaceId is for each one i send in
+        @Override
+        protected void onPostExecute(String input) {
+            if(input != null){
+                Log.i(TAG, "in onPostExecute with String : " + input);
+                Places.GeoDataApi.getPlaceById(mGoogleApiClient, input)
                         .setResultCallback(new ResultCallback<PlaceBuffer>() {
                             @Override
                             public void onResult(PlaceBuffer places) {
                                 if (places.getStatus().isSuccess() && places.getCount() > 0) {
                                     final Place myPlace = places.get(0);
+                                    // debug items
                                     Log.i(TAG, "The number of places in the array is: " + places.getCount());
                                     Log.i(TAG, "Place found: " + myPlace.getName());
                                     Log.i(TAG, "Place address: " + myPlace.getAddress());
@@ -117,20 +121,32 @@ public class ParkDetail extends AppCompatActivity implements OnMapReadyCallback,
                                     Log.i(TAG, "URL is: " + myPlace.getWebsiteUri());
                                     Log.i(TAG, "Price level is: " + myPlace.getPriceLevel());
                                     Log.i(TAG, "Rating is: " + myPlace.getRating());
+                                    // setting text of textviews here
+                                    parkName.setText(selectedPark.getName());
+                                    parkPhone.setText("Phone: " + myPlace.getPhoneNumber());
+                                    parkAddress.setText("Address: " + myPlace.getAddress());
+                                    amenitiesList.setText("Amenities: " + selectedPark.amenitiesList());
+//                                    TextView parkAddress = (TextView)findViewById(R.id.textView7);
+//                                    parkAddress.setText(myPlace.getAddress());
+//                                    parkPhone = (TextView)findViewById(R.id.textView3);
+//                                    parkPhone.setText("Phone number: " + myPlace.getPhoneNumber());
+//                                    TextView amenitiesList = (TextView)findViewById(R.id.textView8);
+//                                    amenitiesList.setText("Amenities: " + selectedPark.amenitiesList());
+//                                    Log.i(TAG, "Amenities: " + selectedPark.amenitiesList());
+
+                                    // this ensures we don't have a memory leak
+                                    places.release();
+                                    mGoogleApiClient.disconnect();
                                 } else {
                                     Log.e(TAG, "Place not found");
                                 }
-                                places.release();
-                                mGoogleApiClient.disconnect();
                             }
                         });
             }
-            return autocompletePredictions.get(0).getPlaceId();
-        }
+            else{
+                Log.e(TAG, "do in Background returned null");
+            }
 
-        @Override
-        protected void onPostExecute(String input) {
-            Log.i(TAG, "in onPostExecute with String : " + input);
         }
     }
 
